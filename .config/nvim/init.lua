0ef6e04301efa3c8395ee776f900f0da27f11a6b
Daniel <danielc2501@gmail.com>
2025-05-06 09:45:06 -0300

    set linebreak as default in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -95,6 +95,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- setting up linebreaking by default
+vim.wo.linebreak = true
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
